238c19a7aa966ede1ee5a99c16b4e87ab8fa580a
kappa44 <kappa44@users.noreply.github.com>
2016-03-13 21:13:13 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-L.mapbox.accessToken = "pk.eyJ1IjoiYnJ5bWNicmlkZSIsImEiOiJXN1NuOFFjIn0.3YNvR1YOvqEdeSsJDa-JUw";
+L.mapbox.accessToken = "pk.eyJ1Ijoia2FwcGFkIiwiYSI6ImNpbHByN2MyaDA4azV0cmtuN2ViZHJjZGQifQ.HOz6XbnWiLlmYQkqMtwVaQ";
 
 var titleField, cluster, userFields = [], urlParams = {};
 
